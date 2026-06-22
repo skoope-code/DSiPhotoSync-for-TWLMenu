e539cb09600ab6d28af9088e4960c5a95ae1027b
skoope-code <mariocostantini0@gmail.com>
2026-06-22 14:41:11 -0500

    Add files via upload

---
+++ picojpeg.d
@@ -0,0 +1,4 @@
+picojpeg.o: \
+ /Users/mariocostantini/Downloads/dsiphotosync-73/source/picojpeg.c \
+ /Users/mariocostantini/Downloads/dsiphotosync-73/source/picojpeg.h
+/Users/mariocostantini/Downloads/dsiphotosync-73/source/picojpeg.h:
---
+++ puff.d
@@ -0,0 +1,3 @@
+puff.o: /Users/mariocostantini/Downloads/dsiphotosync-73/source/puff.c \
+ /Users/mariocostantini/Downloads/dsiphotosync-73/source/puff.h
+/Users/mariocostantini/Downloads/dsiphotosync-73/source/puff.h:
