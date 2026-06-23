5e299501f140520cb053057837f4e96c7c6bb5a7
skoope-code <mariocostantini0@gmail.com>
2026-06-22 20:14:18 -0500

    Delete picojpeg.d

--- picojpeg.d
+++
@@ -1,4 +0,0 @@
-picojpeg.o: \
- /Users/mariocostantini/Downloads/dsiphotosync-73/source/picojpeg.c \
- /Users/mariocostantini/Downloads/dsiphotosync-73/source/picojpeg.h
-/Users/mariocostantini/Downloads/dsiphotosync-73/source/picojpeg.h:
